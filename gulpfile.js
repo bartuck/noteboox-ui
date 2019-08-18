3274b9f83063d0845f72e0bee2c781affa03402e
Bartosz Antonowicz <antonowicz.bartosz@gmail.com>
2019-08-18 19:53:19 +0200

    Add translations config to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const PORT = 3000;
 const SRC_DIR = 'src';
 const OUT_DIR = 'dist';
 const WEBPACK_OUT_DIR = OUT_DIR + '/';
+const SRC_TRANS = 'pl_PL.json'; // If it's changed on watch, you have to call $ gulp watch again
+const DEFAULT_TASKS = [cleanDest, copy, translate, processJs, processCss];
 
 // Imports
 const gulp = require('gulp');
@@ -13,6 +15,7 @@ const webpack = require('webpack-stream');
 const browserSync = require('browser-sync');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+const mustache = require("gulp-mustache");
 
 sass.compiler = require('node-sass');
 
@@ -38,7 +41,6 @@ function reload(next) {
 
 function copy() {
   return gulp.src([
-    SRC_DIR + '/*.html',
     SRC_DIR + '/**/*.jpg'
   ]).pipe(gulp.dest(OUT_DIR));
 }
@@ -63,13 +65,29 @@ function processCss() {
     .pipe(gulp.dest(OUT_DIR + '/css/'));
 }
 
+function translate() {
+  return gulp.src(SRC_DIR + '/*.html')
+    .pipe(mustache(SRC_DIR + '/translations/' + SRC_TRANS, {}, {}))
+    .pipe(gulp.dest('./' + OUT_DIR));
+}
+
 function watch() {
-  gulp.watch(SRC_DIR + '/*.html', gulp.series(copy, reload));
+  gulp.watch(SRC_DIR + '/*.jpg', gulp.series(copy, reload));
+  gulp.watch(SRC_DIR + '/*.html', gulp.series(translate, reload));
+  gulp.watch(SRC_DIR + '/translations/*.json', gulp.series(translate, reload));
   gulp.watch(SRC_DIR + '/scss/**/*.scss', gulp.series(processCss, reload));
   gulp.watch(SRC_DIR + '/js/*.js', gulp.series(processJs, reload));
 }
 
-// Task Series
-gulp.task('build', gulp.series(cleanDest, copy, processJs, processCss));
-gulp.task('serve', gulp.series(cleanDest, copy, processJs, processCss, serve));
-gulp.task('watch', gulp.series(cleanDest, copy, processJs, processCss, gulp.parallel(serve, watch)));
+/**
+ * Tasks
+ */
+
+// Just build and finish
+gulp.task('build', gulp.series(DEFAULT_TASKS));
+
+// Build and serve
+gulp.task('serve', gulp.series(DEFAULT_TASKS, serve));
+
+// Build, serve and watch for changes
+gulp.task('watch', gulp.series(DEFAULT_TASKS, gulp.parallel(serve, watch)));
