6c5e4aceeada9101c3840a84b3a75d307706e640
Bartosz Antonowicz <antonowicz.bartosz@gmail.com>
2019-09-13 20:03:24 +0200

    Add button close

--- src/js/components/note-content/note-content.component.js
+++ src/js/components/note-content/note-content.component.js
@@ -1,8 +1,9 @@
 import { APP_ROUTER } from "../../core/const/app-router.const";
 import { APP_EVENTS } from "../../core/const/app-events.const";
+import { AppRouter } from "../../core/app-router.core";
 
 export class NoteContentComponent {
-  constructor(eventManager, params, query) {
+  constructor(eventManager, params) {
     this.eventManager = eventManager;
     this.params = params;
 
@@ -23,8 +24,15 @@ export class NoteContentComponent {
   }
 
   show() {
-    console.log('NoteContentComponent');
     this.noteHTML.innerHTML = this.createContentHTML();
+
+    this.appendCloseBtnHTML();
+  }
+
+  close() {
+    this.noteHTML.innerHTML = '';
+
+    this.redirectToNotesList();
   }
 
   createContentHTML() {
@@ -32,10 +40,35 @@ export class NoteContentComponent {
       title: 'Mock Title',
       content: 'Mock Note Contents'
     };
-    let contentHTML = `<h3>${contentMock.title}</h3>`
+    let contentHTML = `<h3>${contentMock.title}</h3>`;
 
     contentHTML += `<p>${contentMock.content}</p>`;
 
     return contentHTML;
+  }
+
+  createCloseBtnHTML() {
+    const btnHTML = document.createElement('button');
+
+    btnHTML.type = 'button';
+    btnHTML.innerHTML = '&cross;';
+    btnHTML.addEventListener('click', () => this.close());
+
+    return btnHTML;
+  }
+
+  appendCloseBtnHTML() {
+    const btnHTML = this.createCloseBtnHTML();
+
+    this.noteHTML.appendChild(btnHTML);
+  }
+
+  redirectToNotesList() {
+    console.log(this.params);
+    const noteId = this.params[APP_ROUTER.NOTE.PARAMS.ID];
+    const notebookId = this.params[APP_ROUTER.NOTEBOOK.PARAMS.ID];
+    const pathCurrNoteNotes = AppRouter.getPathToNote(noteId, notebookId);
+
+    console.log('pathCurrNoteNotes', pathCurrNoteNotes);
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -15,9 +15,7 @@ class AppCore {
     this.router = new AppRouter().router;
 
     new AppInstallPrompt();
-    new NotebooksComponent(this.eventManager);
 
-    this.dispatchInitialEvents();
     this.initRouters();
   }
 
@@ -25,48 +23,29 @@ class AppCore {
    * Remember to add a hash in http:// to the paths
    */
   initRouters() {
-    console.log(AppRouter.getPathToNotebooks());
-
     this.router
       .on(AppRouter.getPathToNotebooks(), () => {
-        console.log('#1');
+        new NotebooksComponent(this.eventManager);
+
         this.eventManager.dispatchEvent(APP_EVENTS.SHOW_NOTEBOOKS);
       })
       .on(AppRouter.getPathToNotebook(), (params, query) => {
-        console.log('#2');
+        new NotebooksComponent(this.eventManager);
         new NotesComponent(this.eventManager, params, query);
 
+        this.eventManager.dispatchEvent(APP_EVENTS.SHOW_NOTEBOOKS);
         this.eventManager.dispatchEvent(APP_EVENTS.SHOW_NOTES);
       })
       .on(AppRouter.getPathToNote(), (params, query) => {
-        console.log('#3');
+        new NotebooksComponent(this.eventManager);
+        new NotesComponent(this.eventManager, params, query);
         new NoteContentComponent(this.eventManager, params, query);
 
+        this.eventManager.dispatchEvent(APP_EVENTS.SHOW_NOTEBOOKS);
+        this.eventManager.dispatchEvent(APP_EVENTS.SHOW_NOTES);
         this.eventManager.dispatchEvent(APP_EVENTS.SHOW_NOTE);
       })
       .resolve();
-  }
-
-  dispatchInitialEvents() {
-    this.dispatchNotebooksListEvents();
-    this.dispatchNotesListEvents();
-  }
-
-  dispatchNotebooksListEvents() {
-    if (AppRouter.isNotebooksList) {
-      console.log('isNotebooksList');
-      this.eventManager.dispatchEvent(APP_EVENTS.SHOW_NOTEBOOKS);
-    }
-  }
-
-  dispatchNotesListEvents() {
-    if (!AppRouter.isNotesList) {
-      return null;
-    }
-
-    console.log('isNoteContent');
-    new NoteContentComponent(this.eventManager);
-    this.eventManager.dispatchEvent(APP_EVENTS.SHOW_NOTE);
   }
 }
 
